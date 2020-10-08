be7111ed7e6a0a112270de5aa0955b695276e09b
Abdurrasyid Mujahid <rasyidmujahid@gmail.com>
2020-10-08 10:47:13 +0700

    login

---
+++ client/src/components/Login.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="6" offset="3" style="margin-left: 25%">
+        <div class="white elevation-2">
+          <v-toolbar flat dense color="cyan" dark>
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+
+          <div class="pl-4 pr-4 pt-2 pb-2">
+            <v-text-field
+              name="email"
+              v-model="email"
+              label="Email">
+            </v-text-field>
+            <br>
+            <v-text-field
+              type="password"
+              name="password"
+              v-model="password"
+              label="Password">
+            </v-text-field>
+            <br>
+            <div class="error" v-html="error" />
+            <br>
+            <v-btn
+              dark
+              class="cyan"
+              @click="login">
+              Login
+            </v-btn>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        const response = await AuthenticationService.login({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data)
+        this.error = null
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+  // ,
+  // watch: {
+  //   email (value) {
+  //     console.log('email has changed', value)
+  //   }
+  // },
+  // mounted () {
+  //   setTimeout(() => {
+  //     this.email = 'hello world'
+  //   }, 2000)
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  color: red;
+}
+</style>
