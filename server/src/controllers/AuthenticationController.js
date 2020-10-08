7d92aaebcf16656ea5ebcb70bf31054886a0d39f
Abdurrasyid Mujahid <rasyidmujahid@gmail.com>
2020-10-08 16:42:59 +0700

    show actual error message instead

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -17,7 +17,7 @@ module.exports = {
     } catch (error) {
       console.error(error)
       res.status(400).send({
-        error: 'This email account is already in use'
+        error: error
       })
     }
     
